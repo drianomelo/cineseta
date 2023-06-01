77701c61fac065e0b95ee011902bf8a842b1e25c
Adriano <adrianosenny@gmail.com>
2023-06-01 13:30:09 -0300

    Criando variáveis tailwind para background-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,17 @@ module.exports = {
     extend: {},
     fontFamily: {
       inter: ["Inter", "sans-serif"],
+      kaisei: ["Kaisei Decol", "serif"],
     },
     backgroundImage: {
       museum: "url('/src/img/museum.jpg')",
+      "img-1": "url('/src/img/1.jpg')",
+      "img-2": "url('/src/img/2.jpg')",
+      "img-3": "url('/src/img/3.jpg')",
+      "img-4": "url('/src/img/4.jpg')",
+    },
+    lineHeight: {
+      h1: "75px",
     },
   },
   plugins: [],
