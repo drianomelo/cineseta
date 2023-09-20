4f9f798f2f39490fd9ca805c3b8b359e7b926cc3
Adriano <adrianosenny@gmail.com>
2023-09-20 12:05:58 -0300

    Refactor: Acrescentando novos campos a tabela "users"

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'primeiro_nome',
+        'sobrenome',
         'email',
         'password',
+        'cpf',
+        'telefone',
+        'data_nascimento',
     ];
 
     /**
