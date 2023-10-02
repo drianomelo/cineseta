234da06dce718301c5f8f1814e0d767f62d9ecda
Tercio França Santos <tercio.franca@hotmail.com>
2023-10-02 07:34:16 -0300

    Criação das models restantes (ainda é necessário configurar)

---
+++ app/Models/Gerente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gerente extends Model
+{
+    use HasFactory;
+}
