8f7cb656717817ea5707e4ea6f63acb94a26e2f3
Tercio França Santos <tercio.franca@hotmail.com>
2023-10-03 21:41:41 -0300

    View de visualização das salas

--- resources/views/sala_show.blade.php
+++ resources/views/sala_show.blade.php
@@ -1,16 +1,18 @@
 @extends('master')
 
 @section('content')
+    <div class="flex items-center justify-between px-10 pt-8">
+        <a class="p-2 font-semibold bg-green-600 rounded-lg text-zinc-50" href="{{ route('salas.index') }}">Voltar para Salas</a>
+    </div>
     <div class="flex items-center justify-center w-full h-screen ">
-        <img class="absolute w-full opacity-5 -z-10" src="{{ $filme->capa }}" alt="Capa do filme {{ $filme->titulo }}">
+
         <div>
             <article class="flex flex-col items-center justify-center w-[600px] p-5 rounded-lg bg-gray-950">
                 <div class="flex items-start gap-4">
-                    <img class="w-2/5 rounded-md" src="{{ $filme->capa }}" alt="Capa do filme {{ $filme->titulo }}">
                     <div>
-                        <h2 class="mb-2 text-xl font-bold text-yellow-500">{{ $filme->titulo }}</h2>
-                        <p class="mb-4 font-medium text-justify text-zinc-50">{{ $filme->sinopse }}</p>
-                        <form action="{{ route('filmes.destroy', ['filme' => $filme->id]) }}" method="POST">
+                        <h2 class="mb-2 text-xl font-bold text-yellow-500"> Sala {{ $sala->numero }}</h2>
+                        <p class="mb-4 font-medium text-justify text-zinc-50">Capacidade: {{ $sala->capacidade }} assentos</p>
+                        <form action="{{ route('salas.destroy', ['sala' => $sala->id]) }}" method="POST">
                             @csrf
                             <input type="hidden" name="_method" value="DELETE">
                             <button class="px-2 py-1 font-bold bg-red-800 rounded-lg text-zinc-200"
