781cb9180af815ff9c608ca5c189683e94f0cce6
Tercio França Santos <tercio.franca@hotmail.com>
2023-10-03 21:31:05 -0300

    Botões de módulo na view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,14 @@
 @extends('master')
+
+
+<div class="flex items-center justify-between px-10 pt-8">
+<a class="p-2 font-semibold bg-green-600 rounded-lg text-zinc-50" href="{{ route('salas.index') }}">Gerenciar Salas</a>
+</div>
+
+<div class="flex items-center justify-between px-10 pt-8">
+    <a class="p-2 font-semibold bg-green-600 rounded-lg text-zinc-50" href="{{ route('filmes.index') }}">Gerenciar Filmes</a>
+</div>
+
+<div class="flex items-center justify-between px-10 pt-8">
+    {{-- <a class="p-2 font-semibold bg-green-600 rounded-lg text-zinc-50" href="{{ route('sessoes.index') }}">Gerenciar Sessões</a> --}}
+</div>
